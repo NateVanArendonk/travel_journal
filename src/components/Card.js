8ca6ebba3ca7e57b0edd7b9b06abf4795b032676
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-06-24 11:05:24 -0400

    Add in location icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 export default function Card() {
     return (
         <div className="card">
             <div className="card--img-box">
                 <img className="card--img" src="https://source.unsplash.com/WLxQvbMyfas" alt="TestTitle"></img>
             </div>
+
+            <div className="card--info">
+                <FontAwesomeIcon icon={faLocationDot} className="card--dot" />
+                <h3>Test</h3>
+            </div>
+
+
         </div>
     )
 }
