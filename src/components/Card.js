d57ffe3954c7a7bd287d10a710c88f438ee03bb6
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-06-25 10:21:29 -0400

    Add remaning elements to card html

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,10 +10,17 @@ export default function Card() {
 
             <div className="card--info">
                 <FontAwesomeIcon icon={faLocationDot} className="card--dot" />
-                <h3>Test</h3>
+                <h2 className="card--location">Location Name</h2>
+                <a className="card--location-url" href="https://www.google.com/maps/@43.0233862,-86.1650014,15z?entry=ttu">
+                    View on google maps
+                </a>
+
+                <h3 className="card--title">Title</h3>
+                <p className="card--date">
+                    Date 1 through Date 2
+                </p>
+                <p className="card--description">Description of the trip</p>
             </div>
-
-
         </div>
     )
 }
