981ed3ad4be2d9b81f0c1ae4d7d06544b488d5ff
Nate VanArendonk <nathan.vanarendonk@gmail.com>
2023-06-25 10:22:40 -0400

    Minor edit to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
     return (
         <footer className='foot--text'>
-            React.js learning tool - built by N. VanArendonk
+            React.js learning tool ~ Built by N. VanArendonk
         </footer>
     )
 }
